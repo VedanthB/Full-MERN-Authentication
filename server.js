b3021bcdddd9da540d9ebc266005de1985ed3f9e
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2021-07-16 02:15:52 +0530

    connected to mongoDB

--- server.js
+++ server.js
@@ -1,23 +1,41 @@
-require('dotenv').config();
-const express = require('express')
-const mongoose = require('mongoose')
-const cors = require('cors')
-const cookieParser = require('cookie-parser')
-const fileUpload = require('express-fileupload')
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const fileUpload = require("express-fileupload");
 
-const app = express()
-app.use(express.json())
-app.use(cors())
-app.use(cookieParser())
-app.use(fileUpload({
-    useTempFiles: true
-}))
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
+app.use(
+  fileUpload({
+    useTempFiles: true,
+  })
+);
 
-app.use('/', (req, res, next) => {
-    res.json({msg: "Hello Everyone"})
-})
+app.use("/", (req, res, next) => {
+  res.json({ msg: "Hello Everyone" });
+});
 
-const PORT = process.env.PORT || 8000
+// Connect to mongodb
+const URI = process.env.MONGODB_URL;
+mongoose.connect(
+  URI,
+  {
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err) => {
+    if (err) throw err;
+    console.log("Connected to mongodb");
+  }
+);
+
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
-    console.log("Server is Running on port", PORT)
-})
+  console.log("Server is Running on port", PORT);
+});
