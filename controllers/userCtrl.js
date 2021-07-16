ef744a8d400abb394b78546fc290c64b16bd7f41
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2021-07-16 21:23:17 +0530

    send mail with nodemailer and oauth

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -1,6 +1,15 @@
 const Users = require("../models/userModel");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const sendMail = require("./sendMail");
+
+const { google } = require("googleapis");
+const { OAuth2 } = google.auth;
+const fetch = require("node-fetch");
+
+const client = new OAuth2(process.env.MAILING_SERVICE_CLIENT_ID);
+
+const { CLIENT_URL } = process.env;
 
 const userCtrl = {
   register: async (req, res) => {
@@ -30,10 +39,10 @@ const userCtrl = {
         password: passwordHash,
       };
 
-    //   const activation_token = createActivationToken(newUser);
+      const activation_token = createActivationToken(newUser);
 
-    //   const url = `${CLIENT_URL}/user/activate/${activation_token}`;
-    //   sendMail(email, url, "Verify your email address");
+      const url = `${CLIENT_URL}/user/activate/${activation_token}`;
+      sendMail(email, url, "Verify your email address");
 
       res.json({
         msg: "Register Success! Please activate your email to start.",
