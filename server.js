a9d8774a171e62a72b07d75c2e2251dd17a65537
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2021-07-18 07:03:05 +0530

    make it better

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const fileUpload = require("express-fileupload");
+const path = require("path");
 
 const app = express();
 app.use(express.json());
@@ -35,7 +36,14 @@ mongoose.connect(
   }
 );
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
+}
+
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
-  console.log("Server is Running on port", PORT);
+  console.log("Server is running on port", PORT);
 });
